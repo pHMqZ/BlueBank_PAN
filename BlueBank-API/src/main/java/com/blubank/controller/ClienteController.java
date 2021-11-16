de930556283b83ba6d12224e861384c473dc5ca0
pHMqZ <phillipmarq@hotmail.com>
2021-11-16 19:40:06 -0300

    Começando a implementar Controller Cliente

--- BlueBank-API/src/main/java/com/blubank/controller/ClienteController.java
+++ BlueBank-API/src/main/java/com/blubank/controller/ClienteController.java
@@ -1,20 +1,54 @@
 package com.blubank.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.blubank.model.Cliente;
+import com.blubank.model.DadoCliente;
+import com.blubank.model.Movimento;
+import com.blubank.repository.ClienteRepository;
+import com.blubank.repository.DadoClienteRepository;
+import com.blubank.repository.MovimentoRepository;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private DadoClienteRepository dadoClienteRepository;
+	
 	//o que cliente faz:
-	//cadastro de cliente
+	
+	@PostMapping //("/cadastro/clientes") 
+	public ResponseEntity<Cliente> post(@RequestBody Cliente cliente){
+		return ResponseEntity.status(HttpStatus.CREATED).body(clienteRepository.save(cliente));
+	}
+	
+	@GetMapping
+	public List<DadoCliente> findAll(){
+		return  dadoClienteRepository.findAll();
+		
+	}
+	
+	@GetMapping(value = "/editar/clientes")
+	public ResponseEntity<List<DadoCliente>> getById(){
+		
+	}
 	//editar dados cliente
 	//salvar os dados
 	//excluir dados
 	//movimentação de conta(deposito/transferencia)
 	//hist de movimentação
 	
-	
-
 }
