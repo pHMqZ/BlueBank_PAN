63e195bad15a7d429f3120721846435c329b7ceb
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-11-16 21:05:07 -0300

    Resolvendo problema controller

--- BlueBank-API/src/main/java/com/blubank/controller/ClienteController.java
+++ BlueBank-API/src/main/java/com/blubank/controller/ClienteController.java
@@ -6,5 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-
+//o que cliente faz:
+	//cadastro de cliente
+	//editar dados cliente
+	//salvar os dados
+	//excluir dados
+	//movimentação de conta(deposito/transferencia)
+	//hist de movimentação
 }
