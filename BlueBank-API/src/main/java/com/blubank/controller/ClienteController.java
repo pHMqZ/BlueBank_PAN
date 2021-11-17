374473abe4d936deab1bbe1a9e057b8ebffe05c3
Vinicius Trindade <vinicius.ribeiro@ccc.ufcg.edu.br>
2021-11-16 21:11:06 -0300

    resolvendo

--- BlueBank-API/src/main/java/com/blubank/controller/ClienteController.java
+++ BlueBank-API/src/main/java/com/blubank/controller/ClienteController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-//o que cliente faz:
+
+    //o que cliente faz:
 	//cadastro de cliente
 	//editar dados cliente
 	//salvar os dados
 	//excluir dados
 	//movimentação de conta(deposito/transferencia)
 	//hist de movimentação
+
 }
