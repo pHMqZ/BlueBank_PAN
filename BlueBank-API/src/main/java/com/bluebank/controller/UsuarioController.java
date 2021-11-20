810959a818f50a27d072f5ccb535b65338250b7a
pHMqZ <phillipmarq@hotmail.com>
2021-11-20 13:57:46 -0300

    Criando Branch_test

--- BlueBank-API/src/main/java/com/bluebank/controller/UsuarioController.java
+++ BlueBank-API/src/main/java/com/bluebank/controller/UsuarioController.java
@@ -10,70 +10,7 @@ public class UsuarioController {
 	
 
 //	// o que cliente faz:
-//
-//	@Autowired
-//	private UsuarioRepository usuarioRepo;
-//
-//	@Autowired
-//	private DadoUsuarioRepository dadoClienteRepo;
-//
-//	// @Autowired
-//	// private MovimentoRepository movimentoRepo;
-//
-//	// salvar os dados
-//	@PostMapping // endpoint
-//	public void salvaCliente(@RequestBody Usuario cliente, @RequestBody DadoUsuario dado) {
-//
-//		cliente.setNome(cliente.getNome());
-//		dado.setCpf(dado.getCpf());
-//		dado.setRua(dado.getRua());
-//		dado.setNumero(dado.getNumero());
-//		dado.setComplemento(dado.getComplemento());
-//		dado.setBairro(dado.getBairro());
-//		dado.setCidade(dado.getCidade());
-//		dado.setEstado(dado.getEstado());
-//		dado.setTelefone(dado.getTelefone());
-//		dado.setEmail(dado.getEmail());
-//
-//		clienteRepo.save(cliente);
-//		dadoClienteRepo.save(dado);
-//	}
-//
-//	// editar dados cliente
-//	@PutMapping
-//	public void atualizaCliente(@PathVariable Integer id, @RequestBody Usuario cliente, @RequestBody DadoUsuario dado)
-//			throws Exception {
-//
-//		var c = clienteRepo.findById(id);
-//		var d = dadoClienteRepo.findById(id);
-//
-//		if (c.isPresent()) {
-//			var clienteUpdate = cliente;
-//			clienteUpdate.setNome(cliente.getNome());
-//
-//			clienteRepo.save(clienteUpdate);
-//		} else {
-//			throw new Exception("Cliente não cadastrado");
-//		}
-//
-//		if (d.isPresent()) {
-//			var dadoUpdate = dado;
-//			dadoUpdate.setCpf(dado.getCpf());
-//			dadoUpdate.setRua(dado.getRua());
-//			dadoUpdate.setNumero(dado.getNumero());
-//			dadoUpdate.setComplemento(dado.getComplemento());
-//			dadoUpdate.setBairro(dado.getBairro());
-//			dadoUpdate.setCidade(dado.getCidade());
-//			dadoUpdate.setEstado(dado.getEstado());
-//			dadoUpdate.setTelefone(dado.getTelefone());
-//			dadoUpdate.setEmail(dado.getEmail());
-//
-//			dadoClienteRepo.save(dadoUpdate);
-//		} else {
-//			throw new Exception("Cliente não cadastrado");
-//		}
-//
-//	}
+
 //
 //	// excluir dados
 //	@DeleteMapping
