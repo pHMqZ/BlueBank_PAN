9687bbfc8928712060b84e2edd65f722a507f455
Ordep097 <pedroroseno97@outlook.com>
2021-12-11 23:52:02 -0300

    update run docker

--- front-end/src/services/bases.js
+++ front-end/src/services/bases.js
@@ -1,2 +1,2 @@
-const BASE_URL = "http://localhost:8080/";
+const BASE_URL = "http://api:8080/";
 export default BASE_URL;
--- src/main/java/com/bluebank/config/AwsSnsConfig.java
+++ src/main/java/com/bluebank/config/AwsSnsConfig.java
@@ -1,31 +1,31 @@
-package com.bluebank.config;
-
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
-import lombok.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.AmazonSNSClientBuilder;
-
-@Configuration
-public class AwsSnsConfig {
-    @Value("${}")
-	public MysqlxDatatypes.Scalar.String SECRET_KEY;
-	public MysqlxDatatypes.Scalar.String ACCESS_KEY;
-
-	@Primary
-	@Bean
-	public AmazonSNSClient getSnsClient() {
-
-		return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
-				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
-				.build();
-
-	}
-
-}
+//package com.bluebank.config;
+//
+//import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+//import lombok.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.regions.Regions;
+//import com.amazonaws.services.sns.AmazonSNSClient;
+//import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+//
+//@Configuration
+//public class AwsSnsConfig {
+////    @Value("${}")
+//	public String SECRET_KEY;
+//	public String ACCESS_KEY;
+//
+//	@Primary
+//	@Bean
+//	public AmazonSNSClient getSnsClient() {
+//
+//		return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
+//				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
+//				.build();
+//
+//	}
+//
+//}
